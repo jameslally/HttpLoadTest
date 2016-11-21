66426b152c30152dbb5f3ee480162ea8d8a66e65
James Lally <jamesl@HIQ.local>
2016-11-21 11:11:02 +1100

    Added in a test runner and a .sln file for VS2015

---
+++ src/ConsoleTestRunner/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HttpLoadTester.Entites.Test;
+
+namespace ConsoleTestRunner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var test = new HttpLoadTester.Services.Scenarios.PFMAddNotes();
+            var testRunner = new HttpLoadTester.Services.ScenariosRunner(test , 1 , 1);
+
+            var results = new ConcurrentBag<TestResult> ();
+            testRunner.ExecuteTestRun(results);
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
