f52a54249bf05f591f75b579228f1f51baa6404a
James Lally <jamesl@HIQ.local>
2016-11-23 12:01:30 +1100

    Prepopulated chart

--- src/HttpLoadTester/wwwroot/js/site.js
+++ src/HttpLoadTester/wwwroot/js/site.js
@@ -26756,11 +26756,37 @@ function getQueryVariable(variable) {
 var activeTransport = getQueryVariable('transport') || 'auto';
 var isJsonp = getQueryVariable('jsonp') === 'true';
 
+function prePopulateChartyData(points){
+    var d = new Date();
+    var h = d.getHours() -1;
+    var m = d.getMinutes();
+
+    for (i = 0; i < 60; i++) {
+        m++;
+        if (m > 59){
+            m = 0;
+            h = h + 1;
+            if (h > 23){
+                h = 0;
+            }
+        }
+            points.push({
+                x: moment("" + h + ":" + m, "HH:mm"),
+                y: 0
+            });
+    }
+
+    
+}
+
 function createCharty(containerSelector) {
 
     var charty = {};
+    charty.container = containerSelector;
     charty.dataPoints = [];
     charty.dataPointsFailed = [];
+    prePopulateChartyData(charty.dataPoints);
+
     charty.chart = new Chart(containerSelector, {
         type: 'line',
         data: {
@@ -26770,7 +26796,7 @@ function createCharty(containerSelector) {
                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
                 borderColor: 'rgba(54, 162, 235, 1)'
             }
-            ,{
+                , {
                 label: "Failed",
                 data: charty.dataPointsFailed,
                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
@@ -26795,36 +26821,37 @@ function createCharty(containerSelector) {
         }
     });
 
-    charty.updateChart = function (successData,failedData) {
+    charty.updateChart = function (successData, failedData) {
 
         charty.dataPoints.length = 0;
         charty.dataPointsFailed.length = 0;
 
 
-        successData.forEach(function(element) {
+        successData.forEach(function (element) {
             charty.dataPoints.push({
-                x: moment(element.x,"HH:mm"),
-                y: element.y});
+                x: moment(element.x, "HH:mm"),
+                y: element.y
+            });
         }, this);
 
-        failedData.forEach(function(element) {
+        failedData.forEach(function (element) {
             charty.dataPointsFailed.push({
-                x: moment(element.x,"HH:mm"),
-                y: element.y});
+                x: moment(element.x, "HH:mm"),
+                y: element.y
+            });
         }, this);
 
         charty.chart.update();
     };
 
-
     return charty;
 }
 
 function createDurationCharty(containerSelector) {
-
     var charty = {};
     charty.dataPoints = [];
-
+    prePopulateChartyData(charty.dataPoints);
+    charty.container = containerSelector;
     charty.chart = new Chart(containerSelector, {
         type: 'line',
         data: {
@@ -26851,17 +26878,19 @@ function createDurationCharty(containerSelector) {
             }
         }
     });
-    
+
     charty.updateChart = function (data) {
 
         charty.dataPoints.length = 0;
-        data.forEach(function(element) {
+        data.forEach(function (element) {
             charty.dataPoints.push({
-                x: moment(element.x,"HH:mm"),
-                y: element.y});
+                x: moment(element.x, "HH:mm"),
+                y: element.y
+            });
         }, this);
 
         charty.chart.update();
+
     };
 
 
